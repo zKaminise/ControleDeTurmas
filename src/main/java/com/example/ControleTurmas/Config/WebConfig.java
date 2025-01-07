97001a85fdb33e8e8b38a18b97c6d95c0f37cc79
gabriel.kaminise <gabriel.kaminise@sankhya.com.br>
2025-01-07 19:27:03 -0300

    Ajustando Bug de CORS

--- src/main/java/com/example/ControleTurmas/Config/WebConfig.java
+++ src/main/java/com/example/ControleTurmas/Config/WebConfig.java
@@ -1,18 +1,18 @@
-//package com.example.ControleTurmas.Config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:5173", "https://controledeturmas-production.up.railway.app", "https://controle-turmas.vercel.app", "https://formulario-turmas.vercel.app")
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                .allowedHeaders("*")
-//                .allowCredentials(true);
-//    }
-//}
+package com.example.ControleTurmas.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173", "https://controledeturmas-production.up.railway.app", "https://controle-turmas.vercel.app", "https://formulario-turmas.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
